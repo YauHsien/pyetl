810b8b5bbf140bd208cb3a98b877686b2cb61e06
Yau-Hsien Huang <yauhsienhuang@gmail.com>
2016-07-02 16:24:54 +0800

    fixing typo

--- csv/helpers.py
+++ csv/helpers.py
@@ -3,4 +3,4 @@ from .core import *
 # ssv: semi-colon-separated-values
 # return: {'size': n+1, 0: v0, 1: v1, ..., n: vn}
 def simple_ssv(data):
-    return split(data, ';\n')
+    return split(data, ';')
