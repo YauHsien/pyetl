eaf34b615fcaee0ccdee2dbac6e437ef889b49a5
Yau-Hsien Huang <yauhsienhuang@gmail.com>
2016-07-04 00:18:45 +0800

    adding another test with good performance

--- sqns_access/core.py
+++ sqns_access/core.py
@@ -8,10 +8,11 @@ def gen_rec(fo, help_func):
     if rt == 'record':
         yield 'record', hdr
         hdr = hdr['header']
-    for buf in get_line(fo):
+    for _, buf in enumerate(get_line(fo)):
         r = lambda i: helpers.record(data= buf[i], header= hdr,
                                      help_func= help_func)
         yield list(map(r, buf))
+        buf = None
 
 def get_line(fo):
     MaxBufSize, buf = 1000000, {}
@@ -28,3 +29,14 @@ def test():
         print(time.time() - t)
         t = time.time()
     fo.close()
+
+def test2():
+    fo = open('../wavein/TWM/ctp_mapping_table_raw.txt', 'r')
+    t = time.time()
+    x = fo.readlines()
+    print(time.time() - t)
+    fo.close()
+    for i, line in enumerate(x):
+        print(line)
+        if i > 8:
+            break
